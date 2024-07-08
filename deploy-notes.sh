32501bf025067bf03a440754b9af4a1497f080a7
Shoaib Ajaib <ajxa@pm.me>
2024-07-08 20:36:39 +0100

    updated the deployment notes to include further info on pushing the local image to the container registry

--- deploy-notes.sh
+++ deploy-notes.sh
@@ -17,14 +17,31 @@ docker run -p 3838:3838 gbmdeconvoluter:final_test
 # by opening it in a web browser at the exposed port location:
 http://localhost:3838/
 
-# next we use docker login to connect to our private registry
+# Once we have ensured the local docker image and container are working as intended we can then
+# push this image to our private container registry on azure. To do this we will need to first install the
+# Azure CLI which can be done using home-brew and then we can login to our private registry using the
+# url which we have already pre-configured:
+
 docker login gbmdeconvoluter.azurecr.io
-# this will prompt for a password and username available on Azure
+
+# this will prompt us for a username and password that can be found on 
+# the Azure container registry portal in the “Access keys” section
 
 # now we create a new tag of the docker image pointing it at the container registry
-docker tag gbmconvoluter:2022-08-02 gbmdeconvoluter.azurecr.io/gbmconvoluter:2022-08-02
+# docker tag gbmconvoluter:2022-08-02 gbmdeconvoluter.azurecr.io/gbmconvoluter:2022-08-02
+
+Docker tag gbmdeconvoluter:2024-07-08_gbmpurity_image gbmdeconvoluter.azurecr.io/gbmdeconvoluter:2024-07-08_gbmpurity_image
 
 # once retagged we can push the image from our machine to the registry
-docker push gbmdeconvoluter.azurecr.io/gbmconvoluter:2022-08-02
+#  docker push gbmdeconvoluter.azurecr.io/gbmconvoluter:2022-08-02
 
-# the docker image is now available in the private container registry on azure
+docker push gbmdeconvoluter.azurecr.io/gbmdeconvoluter:2024-07-08_gbmpurity_image
+
+# the docker image is now available in the private container registry on azure and can be pointed to
+# from the app service deployment options
+# The last known deployment was:
+Image tag: 9429082df816cd51093f32f79011ce9a1b9a4c45
+
+
+
+
